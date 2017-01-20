a055df53aa773d680a3f65a9f97755609f7b2796
smitherd9 <smitherd9@gmail.com>
2017-01-20 10:25:09 -0500

    created public folder, began modifying ethnicity.js to use dropdown menu

--- js/components/ethnicity.js
+++ js/components/ethnicity.js
@@ -3,7 +3,7 @@ import actions from '../actions/index';
 import store from '../store';
 import Display from './display-results';
 import { connect } from 'react-redux';
-import { Button, FormGroup, FormControl, Radio, Grid, Row, Col } from 'react-bootstrap';
+import { Button, SplitButton, MenuItem, FormGroup, FormControl, Radio, Grid, Row, Col } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 
 
@@ -26,9 +26,10 @@ class Ethnicity extends React.Component {
 	}
 
 	selectedState(e){
-		e.preventDefault();
-		console.log(typeof(e.target.value));
+		// e.preventDefault();
+		console.log(typeof(e.target.value));		
 		console.log(e.target.value);
+		console.log(e.target.eventKey);
 		store.dispatch(actions.selectedState(e.target.value));
 	}
 
@@ -51,6 +52,12 @@ class Ethnicity extends React.Component {
 		<form>
 		
 		<FormGroup>
+
+		<SplitButton title="Search by Ethnicity" pullRight id="split-button-pull-right" onSelect={this.selectedState}>
+			<MenuItem value="Asian and Pacific Islander" eventKey="Asian and Pacific Islander">Asian and Pacific Islander</MenuItem>
+    		<MenuItem eventKey="2">Another action</MenuItem>
+    		<MenuItem eventKey="3">Something else here</MenuItem>
+    	</SplitButton>
 
 		<Radio
 			value="Asian and Pacific Islander"
